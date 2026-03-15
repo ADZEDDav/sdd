29a932112d553a7d258e2513360dc98f566e4b95
ADZE KOMI DAVID <davidsonfamilyfamily@gmail.com>
2026-03-15 20:33:01 +0000

    TP1 terminé

--- lib/objects/objects.c
+++ lib/objects/objects.c
@@ -4,3 +4,61 @@
 #include <stdio.h>
 
 // TODO: Implement
+Car *car_create(char *brand, char *model, char *license)
+{
+    Car *car = malloc(sizeof(Car));
+    car->brand = strdup(brand);
+    car->model = strdup(model);
+    car->license = strdup(license);
+    return car;
+}
+
+Customer *customer_create(char *name, char *address)
+{
+    Customer *customer = malloc(sizeof(Customer));
+    customer->name = strdup(name);
+    customer->address = strdup(address);
+    customer->car = NULL;
+    return customer;
+}
+
+Registry *registry_create(Customer **customers, int size)
+{
+    Registry *registry = malloc(sizeof(Registry));
+    registry->customers = customers;
+    registry->size = size;
+    return registry;
+}
+
+void link_car(Car *car, Customer *customer)
+{
+    customer->car = car;
+}
+
+void car_destroy(Car *car)
+{
+    if (!car) return;
+    free(car->brand);
+    free(car->model);
+    free(car->license);
+    free(car);
+}
+
+void customer_destroy(Customer *customer)
+{
+    if (!customer) return;
+    free(customer->name);
+    free(customer->address);
+    free(customer);
+}
+
+void registry_destroy(Registry *registry)
+{
+    if (!registry) return;
+    free(registry);
+}
+
+void print_registry(Registry *registry)
+{
+    
+}
