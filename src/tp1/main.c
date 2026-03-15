83a991e0e630c09df6129a98f5568ef125dd0815
ADZE KOMI DAVID <davidsonfamilyfamily@gmail.com>
2026-03-15 20:34:35 +0000

    Ajout du main du TP1

--- src/tp1/main.c
+++ src/tp1/main.c
@@ -4,6 +4,13 @@
 int main()
 {
     // TODO: Implement
+    Customer *c = customer_create("David", "Rue du Lac 12");
+    Car *v = car_create("Toyota", "Corolla", "GE1234697");
+
+    link_car(v, c); 
+
+    printf("Client: %s\nAdresse: %s\n", c->name, c->address);
+    printf("Voiture: %s %s (%s)\n", v->brand, v->model, v->license);
 
     return 0;
 }
